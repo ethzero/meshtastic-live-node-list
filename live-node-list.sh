c369dededf9cda9c3bf926bb366802d2444f549d
ethzero <ethzzero@ethzero.uk>
2024-04-10 22:55:18 +0100

    fix: Multi-node support was broken :/ fix: Added trap if 'meshtastic' binary uncounters an issue fix: Altered format of logging for clarity

--- live-node-list.sh
+++ live-node-list.sh
@@ -39,13 +39,16 @@ rc_path=$(check_rc_file "$rc_file")
 
 declare -A NODE_NAME
 declare -A LOCAL_NODE_DUMP_FILE
+declare -A LOCAL_IP_ADDRESS
 declare -A REMOTE_HTML_FILE
 declare -A LOCAL_PORT
+
 source $rc_path
 
 mkdir -p ${LOCAL_UPLOAD_FILES}
 
 echo "Updating Mesh Node List(s)..." | logger -t $LOGGING_ID
+echo "Using config file: ${rc_path}"
 echo "Copying supporting web files to ${LOCAL_UPLOAD_FILES}" | logger -t $LOGGING_ID
 cp ${TEMPLATE_HTML_PATH}/live-node-list* ${LOCAL_UPLOAD_FILES}
 
@@ -59,22 +62,28 @@ do
       echo "... received a subshell ABORT."
       exit 1
   fi
-  echo "Using connection method: ${connection_string}" | logger -t $LOGGING_ID
+  echo "... using connection method: ${connection_string}" | logger -t $LOGGING_ID
 
   ${HOME}/.local/bin/meshtastic $connection_string --nodes | \
     sed "s|Connected to radio|<div id="node-metainfo"><h2 id="node-name">Node: ${NODE_NAME[${key}]}</h2><p id="node-generated">Generated: `date`</p></div><pre>|" \
     > "${LOCAL_UPLOAD_FILES}/${LOCAL_NODE_DUMP_FILE[${key}]}"
+  if [ $? -eq 1 ]; then
+      echo "... meshtastic encountered an issue. ABORTING."
+      exit 1
+  fi
+
+
 
   # If node dump file is not empty, upload it to the webserver and remotely generate the HTML page 
   if [[ -s "${LOCAL_UPLOAD_FILES}/${LOCAL_NODE_DUMP_FILE[${key}]}" ]]; then
-    echo "Creating \"${NODE_NAME[${key}]}\" HTML pages..." | logger -t $LOGGING_ID
+    echo "... creating \"${NODE_NAME[${key}]}\" HTML pages..." | logger -t $LOGGING_ID
     cat \
       ${TEMPLATE_HTML_PATH}/${TEMPLATE_HTML_HEADER} \
       ${LOCAL_UPLOAD_FILES}/${LOCAL_NODE_DUMP_FILE[${key}]} \
       ${TEMPLATE_HTML_PATH}/${TEMPLATE_HTML_FOOTER} \
       > ${LOCAL_UPLOAD_FILES}/${REMOTE_HTML_FILE[${key}]}
 
-    echo "Uploading ${REMOTE_HTML_FILE[${key}]} to ${REMOTE_HOST}..." | logger -t $LOGGING_ID
+    echo "... uploading ${REMOTE_HTML_FILE[${key}]} to ${REMOTE_HOST}..." | logger -t $LOGGING_ID
 
 sftp -q ${REMOTE_USERNAME}@${REMOTE_HOST}:${REMOTE_PATH} <<EOF
 put -r ${LOCAL_UPLOAD_FILES}/*
@@ -84,7 +93,7 @@ EOF
     echo "${LOCAL_NODE_DUMP_FILE[${key}]} is blank. Possible dump error occured"
   fi
 
-  echo "Completed: \"${NODE_NAME[${key}]}\"" | logger -t $LOGGING_ID
+  echo "... completed: \"${NODE_NAME[${key}]}\"" | logger -t $LOGGING_ID
 done
 
 echo "Completed updating nodes list." | logger -t $LOGGING_ID
